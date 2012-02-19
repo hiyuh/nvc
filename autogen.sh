2770f58af61ae937913790791649152b93357f5d
KIMURA Masaru <hiyuh.root@gmail.com>
2012-02-20 03:55:15 +0900

    Prepare for libtool.

--- autogen.sh
+++ autogen.sh
@@ -1,3 +1,13 @@
 aclocal -I m4
+libtoolize --ltdl --copy --force
+# NOTE: cygwin libtool is broken...
+cd libltdl
+	echo "#undef __WINDOWS__" > acconfig.h
+	autoheader
+	sed -i \
+		-e 's/AC_OUTPUT/case $host_os in cygwin* | mingw* | pw32*) AC_DEFINE(__WINDOWS__) ;; esac ; AC_OUTPUT/' \
+		configure.ac
+cd ..
+aclocal -I m4
 autoconf
 automake -a
