70ac7db70935b40309ac117f60bf27374b2b5865
KIMURA Masaru <hiyuh.root@gmail.com>
2012-02-20 11:17:27 +0900

    Fix for libtool preparation.
     * Do not do aclocal before doing libtoolize.
     * Chain libtoolize and required workaround w/ &&
     * Do not use AC_CONFIG_{AUX,MACRO}_DIR for libltdl.

--- autogen.sh
+++ autogen.sh
@@ -1,13 +1,12 @@
-aclocal -I m4
-libtoolize --ltdl --copy --force
 # NOTE: cygwin libtool is broken...
-cd libltdl
-	echo "#undef __WINDOWS__" > acconfig.h
-	autoheader
-	sed -i \
-		-e 's/AC_OUTPUT/case $host_os in cygwin* | mingw* | pw32*) AC_DEFINE(__WINDOWS__) ;; esac ; AC_OUTPUT/' \
-		configure.ac
-cd ..
+libtoolize --ltdl --copy --force && \
+	cd libltdl && \
+		echo "#undef __WINDOWS__" > acconfig.h && \
+		autoheader && \
+		sed -i \
+			-e 's/AC_OUTPUT/case $host_os in cygwin* | mingw* | pw32*) AC_DEFINE(__WINDOWS__) ;; esac ; AC_OUTPUT/' \
+			configure.ac && \
+	cd ..
 aclocal -I m4
 autoconf
 automake -a
