704134f5053336b7e7f9ed1d7717c3f1da0e4a6c
KIMURA Masaru <hiyuh.root@gmail.com>
2012-02-21 02:14:12 +0900

    Initial working version on cygwin!
     * autogen.sh
       No need to run libtoolize w/ --ltdl.
       Do autoreconf.
     * configure.ac
       Move libtool's black magic to use only for WIN32.
       Also note why this trick is required.
     * src/Makefile.am
       Make stub DLL on WIN32.

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,5 @@
 # NOTE: cygwin libtool is broken...
-libtoolize --ltdl --copy --force && \
+libtoolize --copy --force && \
 	cd libltdl && \
 		echo "#undef __WINDOWS__" > acconfig.h && \
 		autoheader && \
@@ -7,6 +7,7 @@ libtoolize --ltdl --copy --force && \
 			-e 's/AC_OUTPUT/case $host_os in cygwin* | mingw* | pw32*) AC_DEFINE(__WINDOWS__) ;; esac ; AC_OUTPUT/' \
 			configure.ac && \
 		autoconf && \
+		autoreconf && \
 	cd ..
 aclocal -I m4
 autoconf
