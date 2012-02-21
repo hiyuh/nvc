0eb7bf25c0cd395181c72bc3d39e7539fa2bd37f
KIMURA Masaru <hiyuh.root@gmail.com>
2012-02-21 18:53:26 +0900

    Remove unneeded libtool black magic.
     * autogen.sh
       Just run libtool, we do not needs libltdl ATM.
     * configure.ac
       Use LT_INIT instead of AC_PROG_RANLIB.
       Remove ton of libtool black magic for libltdl.
     * Makefile.am
       Remove libltdl from SUBDIRS.
     * config.h.in
       Regenerated.
     * .gitignore
       Ignore *.lo which are generated objects by libtool.
       Ignore *.la which are generated archives by libtool.
       Ignore /libtool which is regenerated on host.
       Ignore /ltmain.sh which is regenerated on host.
       Ignore /src/nvc.exe which is the executable one on cygwin.

--- autogen.sh
+++ autogen.sh
@@ -1,14 +1,4 @@
-# NOTE: cygwin libtool is broken...
-libtoolize --copy --force && \
-	cd libltdl && \
-		echo "#undef __WINDOWS__" > acconfig.h && \
-		autoheader && \
-		sed -i \
-			-e 's/AC_OUTPUT/case $host_os in cygwin* | mingw* | pw32*) AC_DEFINE(__WINDOWS__) ;; esac ; AC_OUTPUT/' \
-			configure.ac && \
-		autoconf && \
-		autoreconf && \
-	cd ..
+libtoolize
 aclocal -I m4
 autoconf
 automake -a
