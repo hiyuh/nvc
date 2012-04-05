37d34224c69d0506ab095a7d8b78c618f182cb8f
KIMURA Masaru <hiyuh.root@gmail.com>
2012-04-05 11:49:15 +0900

    Add TRACE() in _assert_fail().

--- src/rt/rtkern.c
+++ src/rt/rtkern.c
@@ -293,6 +293,9 @@ void _assert_fail(const uint8_t *msg, int32_t msg_len, int8_t severity,
    // c) The value of the message string
    // d) The name of the design unit containing the assertion
 
+   TRACE("_assert_fail msg=%s msg_len=%d severity=%d where=%d module=%s",
+         msg, msg_len, severity, where, module);
+
    assert(severity < 4);
 
    const char *levels[] = {
